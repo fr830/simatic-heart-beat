dbad8bf0097a8b067cdeeaccd4f9aedaab0cf9ce
svc_SIMATIC_DEV <svc_SIMATIC_DEV@afsr.mi.valueaddco.com>
2017-11-07 10:27:36 -0800

    fixes catch exception in RTDS manager and adds NLog.dll

--- SimaticHeartBeatService/SimaticHeartBeatService/Business/RtdsManager.cs
+++ SimaticHeartBeatService/SimaticHeartBeatService/Business/RtdsManager.cs
@@ -9,31 +9,24 @@ using System.Configuration;
 
 namespace SimaticHeartBeatService.Business
 {
-  public class RtdsManager
-  {
-    private static Logger logger = LogManager.GetCurrentClassLogger();
-    private string siteName = System.Configuration.ConfigurationManager.AppSettings["siteName"];
-    private string rtdsSourceUnit = System.Configuration.ConfigurationManager.AppSettings["rtdsSourceUnit"];
-    public void updateRtdsTag(string clientName, bool value)
+    public class RtdsManager
     {
-      string tagFullName = rtdsSourceUnit + "\\" + siteName + "_" + clientName + "_HeartBeat_WEB";
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private string siteName = System.Configuration.ConfigurationManager.AppSettings["siteName"];
+        private string rtdsSourceUnit = System.Configuration.ConfigurationManager.AppSettings["rtdsSourceUnit"];
+        public void updateRtdsTag(string clientName, bool value)
+        {
+            string tagFullName = rtdsSourceUnit + "\\" + siteName + "_" + clientName + "_HeartBeat_WEB";
 
-      try
-      {
-        if (RTDS.ExistThrough(tagFullName))
-        {
-          logger.Trace("Updating: " + tagFullName + " value: " + value);
-          RTDS.Write(tagFullName, value);
+            if (RTDS.Write(tagFullName, value))
+            {
+                logger.Trace("Updating: " + tagFullName + " value: " + value);
+            }
+            else
+            {
+                logger.Trace("Error in RTDS tag manipulation: " + tagFullName);
+            }
+
         }
-        else
-        {
-          logger.Trace("Updating Failed: " + tagFullName + " does not exists ");
-        }
-      }
-      catch (Exception ex)
-      {
-        logger.Trace("Error in RTDS tag manipulation: " + ex.ToString());
-      }
     }
-  }
 }
